2b2312cfa8023b9763738850214478317800622a
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-01-27 10:13:13 -0800

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+
+  const h2Style= {
+    color: "firebrick"
+  }
+
+  return <div id ="home"> Home
+    <h1 style= {h2Style} > {name} is a Web Developer from {city} </h1>
+  </div>;
 }
 
 export default Home;
